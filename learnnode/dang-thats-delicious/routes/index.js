ef661c3b7efd68afd6875c94328bad09063ca040
Mohammed <mohammedelzanaty129@gmail.com>
2019-01-28 18:20:33 +0200

    [ C, Learn Node ] Core Concept - Routing

--- learnnode/dang-thats-delicious/routes/index.js
+++ learnnode/dang-thats-delicious/routes/index.js
@@ -3,7 +3,23 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // res.send('Hey! It works!');
+  const info = {
+    name: 'zanaty',
+    age: 25,
+    title: 'Frontend Developer'
+  }
+  res.json(info)
+  // res.send(req.query)
+  // res.status(500).json({error: 'message'})
+  // res.redirect('http://google.com')
+  // res.status(400).send('Bad Request');
 });
+
+router.get('/user/:id', (req, res) => {
+  // res.send('User ' + [...req.params.id].reverse().join(''));
+  console.log(req.baseUrl)
+  res.status(200).send('Sucess')
+})
 
 module.exports = router;
